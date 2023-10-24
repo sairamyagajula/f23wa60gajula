99c4814d72edca2f9db11a9563a30c25aa43288e
sairamyagajula <ramyaroyal1639@gmail.com>
2023-10-24 17:13:57 -0500

    added users.js code

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+// Initialize variables
+var sum = 0;
+var more = 0;
+
+// Define the route
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // Add 1 to 'more'
+  more += 1;
+  // Add 'more' to 'sum'
+  sum += more;
+  // Send the response
+  res.send(`Sum is: ${sum}`);
 });
 
 module.exports = router;
