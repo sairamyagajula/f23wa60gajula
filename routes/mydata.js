3ebda4b084893f32498ec71c61d89dbc41614b1d
sairamyagajula <ramyaroyal1639@gmail.com>
2023-10-24 17:37:05 -0500

    updated mydata code

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,6 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { ti: 'Sai Ramya Gajula' });
+  res.render('mydata', { title: 'Sai Ramya Gajula' });
 });
 module.exports = router;
