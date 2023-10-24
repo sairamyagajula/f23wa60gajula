8a793755d3143e943a70a385104d222d7a0dbb8c
sairamyagajula <ramyaroyal1639@gmail.com>
2023-10-24 16:49:13 -0500

    new endpoint

--- mydata.js
+++ mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Sai Ramya Gajula' });
+  res.render('mydata', { ti: 'Sai Ramya Gajula' });
 });
 
 module.exports = router;
