10b9c822d53ea9c7f5e4db9a2bbb8506649bc42f
sairamyagajula <ramyaroyal1639@gmail.com>
2023-10-24 17:43:31 -0500

    updated users code

--- routes/users.js
+++ routes/users.js
@@ -7,11 +7,8 @@ var more = 0;
 
 // Define the route
 router.get('/', function(req, res, next) {
-  // Add 1 to 'more'
   more += 1;
-  // Add 'more' to 'sum'
   sum += more;
-  // Send the response
   res.send(`Sum is: ${sum}`);
 });
 
